cdcb31c31d199592e6abd294ea0076095706ea97
King-George-Hughes <121091106+King-George-Hughes@users.noreply.github.com>
2023-10-28 23:56:26 +0000

    changes

--- react/src/pages/Login.jsx
+++ react/src/pages/Login.jsx
@@ -11,6 +11,9 @@ const Login = () => {
                 onSubmit={onSubmit}
                 className="bg-gray-50 p-5 w-[400px] h-[350px] flex items-center flex-col justify-evenly"
             >
+                <h2 className="text-gray-800 font-medium text-2xl">
+                    Log into account
+                </h2>
                 <input
                     type="email"
                     placeholder="Email"
@@ -29,7 +32,10 @@ const Login = () => {
                 </button>
                 <p>
                     Not Registered?{" "}
-                    <Link to={"/register"} className="text-purple-700">
+                    <Link
+                        to={"/register"}
+                        className="text-purple-700 font-medium"
+                    >
                         Create an account.
                     </Link>
                 </p>
--- react/src/pages/Register.jsx
+++ react/src/pages/Register.jsx
@@ -1,5 +1,54 @@
+import { Link } from "react-router-dom";
+
 const Register = () => {
-    return <div>Register</div>;
+    const onSubmit = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <div className="w-full h-screen flex items-center justify-center">
+            <form
+                onSubmit={onSubmit}
+                className="bg-gray-50 p-5 w-[400px] h-[450px] flex items-center flex-col justify-evenly"
+            >
+                <h2 className="text-gray-800 font-medium text-2xl">
+                    Create an account
+                </h2>
+                <input
+                    type="text"
+                    placeholder="Name"
+                    className="w-80 h-12 border-2 px-3"
+                />
+                <input
+                    type="email"
+                    placeholder="Email"
+                    className="w-80 h-12 border-2 px-3"
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    className="w-80 h-12 border-2 px-3"
+                />
+                <input
+                    type="password"
+                    placeholder="Comfirm Password"
+                    className="w-80 h-12 border-2 px-3"
+                />
+                <button
+                    type="submit"
+                    className="w-80 h-12 border-2 px-3 bg-purple-700 text-gray-50 text-xl"
+                >
+                    Login
+                </button>
+                <p>
+                    Already a user?{" "}
+                    <Link to={"/login"} className="text-purple-700 font-medium">
+                        Login.
+                    </Link>
+                </p>
+            </form>
+        </div>
+    );
 };
 
 export default Register;
